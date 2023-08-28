dc8af6d1b485fed28906fbcb9034a694e35e1917
KRISHNAsovereign <101880819+KRISHNAsovereign@users.noreply.github.com>
2023-08-28 23:05:36 +0530

    virtual

--- NBV/wsgi.py
+++ NBV/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import os
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NBV.settings')
 
